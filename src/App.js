6bc2c85e350cba02fc0cea58dcf6c57aca744ad0
SC00bidu <you@example.com>
2024-01-03 22:51:54 +0530

    bg & font updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import bg from "./landingbg.png";
+import grid from "./assets/background.png";
 
 function App() {
   return (
     <div
       className="relative"
       style={{
-        backgroundImage: `url(${bg})`,
+        backgroundImage: `url(${grid})`,
         backgroundSize: "cover",
-        backgroundPosition: "top center",
       }}
     >
       <div className="w-screen h-screen"></div>
@@ -19,15 +19,17 @@ function App() {
         <h1 className="myFont md:text-3xl sm:text-2xl text-xl md:mb-8 mb-3 font-bold text-white">
           Coming Soon
         </h1>
-        <p className="md:text-2xl sm:text-lg text-md font-medium text-white">
-          Generate High-Quality AI generated Design prints on
-        </p>
-        <p className="md:text-2xl sm:text-lg text-md font-medium text-white">
-          Fits and order it right away!
-        </p>
-        <p className="md:text-2xl sm:text-lg text-md font-medium text-white">
-          Explore the era of AI generated Fashion.
-        </p>
+        <div className="myFont1">
+          <p className="md:text-2xl sm:text-lg text-md font-medium text-white">
+            Generate High-Quality AI generated Design prints on
+          </p>
+          <p className="md:text-2xl sm:text-lg text-md font-medium text-white">
+            Fits and order it right away!
+          </p>
+          <p className="md:text-2xl sm:text-lg text-md font-medium text-white">
+            Explore the era of AI generated Fashion.
+          </p>
+        </div>
       </div>
     </div>
   );
